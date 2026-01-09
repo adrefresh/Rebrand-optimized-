27452c4af3777ea47bad4ef7ea85be472101172d
Revanth-re <revanthrevi131@gmail.com>
2026-01-09 15:55:27 +0530

    layout changes

--- app/components/service-page-components/adops-contact-hero.tsx
+++ app/components/service-page-components/adops-contact-hero.tsx
@@ -192,7 +192,7 @@ export default function AdOpsContactHero2() {
         className="
           pointer-events-none
           absolute
-          top-[28px]
+          top-[16px]
           left-[50px]
           z-[1]
           h-[70vh]
