44362de7201fe53699dccd32548e37a9e09deaf5
Revanth-re <revanthrevi131@gmail.com>
2026-02-25 16:25:03 +0530

    changes in hero sections

--- app/components/careers-components/jobs.tsx
+++ app/components/careers-components/jobs.tsx
@@ -41,7 +41,7 @@ export default function CareersClient() {
         </h1>
 
         <p className="mt-2 text-gray-600 text-[0.95rem] sm:text-base">
-          Explore exciting opportunities to grow your career with AdRefresh.
+          Explore exciting opportunities to grow your career with AdRefreshh.
         </p>
       </div>
 
