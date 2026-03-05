94c6c39b6dc82ac26c592d95dc5336fbcd8385ae
Revanth-re <revanthrevi131@gmail.com>
2026-03-06 00:06:37 +0530

    OrgSchema Enabled

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,52 +1,5 @@
 
-// import type { Metadata } from "next";
-// import "./globals.css";
-// import Header from "./components/Header";
-// import Footer from "./components/Footer";
-// import CookieBanner from "./components/cookiebanner/cookieBanner";
-// import GoogleTagManagerContainer from "./components/cookiebanner/GoogleTagManager";
 
-// import { Manrope } from "next/font/google";
-// export const metadata: Metadata = {
-//   title: "AdRefresh - Digital Marketing and AdOps",
-//   description: "AI-Enhanced Digital Marketing and Advertising Operations",
-//   icons: {
-//     icon: "/favicon.ico",
-//     shortcut: "/favicon.ico",
-//     apple: "/apple-touch-icon.png",
-//   },
-//   manifest: "/site.webmanifest",
-// };
-
-// export default function RootLayout({
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }) {
-//   const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
-
-//   return (
-//     <html
-//       lang="en"
-//        className={manrope.className}
-//       style={{
-//         ["--base-path" as any]: basePath,
-//       }}
-//     >
-//       <head>
-//         <GoogleTagManagerContainer gtmId="GTM-MMMX5TGB" />
-//       </head>
-//       <body className="layout-root">
-//         {/* Cookie Consent Banner */}
-//         <CookieBanner />
-
-//         <Header />
-//         {children}
-//         <Footer />
-//       </body>
-//     </html>
-//   );
-// }
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "./components/Header";
@@ -54,6 +7,7 @@ import Footer from "./components/Footer";
 import CookieBanner from "./components/cookiebanner/cookieBanner";
 import GoogleTagManagerContainer from "./components/cookiebanner/GoogleTagManager";
 import { Manrope } from "next/font/google";
+import organizationSchema from "@/libraries/schema/organizationSchema";
 
 const manrope = Manrope({
   subsets: ["latin"],
@@ -82,6 +36,13 @@ export default function RootLayout({
     >
       <head>
         <GoogleTagManagerContainer gtmId="GTM-MMMX5TGB" />
+
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify(organizationSchema),
+          }}
+        />
       </head>
 
       <body className="layout-root">
