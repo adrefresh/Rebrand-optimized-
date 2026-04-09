199f2ff20ea0cd3a3344359620ea0a0543357582
adrefresh-revantthh <revanth.b@adrefresh.com>
2026-04-09 16:16:49 +0530

    added space in hero title section

--- app/components/homePageComponents/Hero.tsx
+++ app/components/homePageComponents/Hero.tsx
@@ -41,7 +41,7 @@ export default function Hero() {
 <div className="relative z-10 flex h-full max-w-7xl mx-auto flex-col items-start justify-start px-2 sm:px-24 text-left pt-35 md:pt-18 lg:pt-38 lg:-ml-10">
         <h1 className="w-full text-left text-[2rem] sm:text-[2.2rem] md:text-[4rem] lg:text-[5rem] xl:text-[6rem] -mt-25 sm:mt-4 font-bold sm:tracking-tight leading-tight text-white xl:translate-y-8">
           YOUR REMOTE TEAM<br />
-          THAT FEELS ON SITE
+          THAT FEELS ON SITE{" "}
         <span className="hidden">Advertising Operations, Digital Marketing</span>
         </h1>
       </div>
